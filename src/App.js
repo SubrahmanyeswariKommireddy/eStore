a7fe7082d44130162aa863f016691e1f28d9a336
SubrahmanyeswariKommireddy <subrahmanyeswari.666@gmail.com>
2023-07-28 08:25:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+import TopNav from './Components/TopNav';
+import CatNav from './Components/CatNav';
+import MainComponent from './Components/MainComponent';
+const App = () => {
+  return (
+    <div className='App'>
+    <TopNav />
+    <CatNav />
+    <MainComponent />
+    </div> 
+  );
+};
+
+export default App;
